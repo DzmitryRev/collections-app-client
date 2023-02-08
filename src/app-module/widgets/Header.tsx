e1d7e1c60a0b0a213947948c5b4a48d8b4df98d1
Revchenko Dmitry <reuchankadzmitry@gmail.com>
2023-02-08 23:14:00 +0300

    feat: add padding in Header widget

--- src/app-module/widgets/Header.tsx
+++ src/app-module/widgets/Header.tsx
@@ -1,18 +1,21 @@
 import React from "react";
-import { Box } from "@mui/material";
+import { Box, Divider } from "@mui/material";
 import Logo from "../../shared/components/Logo";
 
 export default function Header() {
   return (
-    <Box>
-      <Logo />
-      {/**
-       * TODO:
-       * search input
-       * user icon with dropdown or signin/signup links
-       * OR
-       * burger menu with search input and user icon with dropdown or signin/signup links
-       */}
-    </Box>
+    <>
+      <Box sx={{ py: 2, px: 1 }}>
+        <Logo />
+        {/**
+         * TODO:
+         * search input
+         * user icon with dropdown or signin/signup links
+         * OR
+         * burger menu with search input and user icon with dropdown or signin/signup links
+         */}
+      </Box>
+      <Divider />
+    </>
   );
 }
