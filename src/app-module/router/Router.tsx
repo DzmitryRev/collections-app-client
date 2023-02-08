90dbd5e9640e1649eb832d0ed828186a4a77b3f0
Revchenko Dmitry <reuchankadzmitry@gmail.com>
2023-02-08 19:06:01 +0300

    feat: router in app-module
    - define router.tsx component;
    - create base structure;

---
+++ src/app-module/router/Router.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Routes } from "react-router-dom";
+
+export default function Router() {
+  return (
+    <Routes>
+      {/**
+       * Routes
+       */}
+    </Routes>
+  );
+}
