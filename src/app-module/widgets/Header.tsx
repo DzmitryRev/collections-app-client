c202841412ae67d3f8e08def94dc459df36a49dd
Revchenko Dmitry <reuchankadzmitry@gmail.com>
2023-02-10 00:40:43 +0300

    feat: connect theme widgets to header

--- src/app-module/widgets/Header.tsx
+++ src/app-module/widgets/Header.tsx
@@ -1,15 +1,51 @@
 import React from "react";
-import { Box } from "@mui/material";
-import Logo from "../../shared/components/Logo";
-import ThemeSwitcher from "./ThemeSwitcher";
+import { Box, BoxProps, Divider, styled, useTheme } from "@mui/material";
+import { Logo } from "../../shared/components";
+import { DefaultThemeSwitcher } from "../../modules/theme-module";
+import AppSettingsMobile from "./AppSettingsMobile";
+
+const HeaderContainerSX = {
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "space-between",
+  py: 2,
+  px: 1,
+};
+
+const HeaderHandlersContainerSX = {
+  display: "flex",
+  justifyContent: "space-between",
+  alignItems: "center",
+};
+
+const MobileSettingsContainerSX = styled(Box)(({ theme }) => ({
+  display: "none",
+  [theme.breakpoints.down("sm")]: {
+    display: "block",
+  },
+}));
+
+const PCSettingsContainerSX = styled(Box)(({ theme }) => ({
+  [theme.breakpoints.down("sm")]: {
+    display: "none",
+  },
+}));
 
 export default function Header() {
   return (
-    <Box
-      sx={{ display: "flex", alignItems: "center", justifyContent: "space-between", py: 2, px: 1 }}
-    >
-      <Logo />
-      <ThemeSwitcher />
-    </Box>
+    <>
+      <Box sx={HeaderContainerSX}>
+        <Logo />
+        <Box sx={HeaderHandlersContainerSX}>
+          <MobileSettingsContainerSX>
+            <AppSettingsMobile />
+          </MobileSettingsContainerSX>
+          <PCSettingsContainerSX>
+            <DefaultThemeSwitcher />
+          </PCSettingsContainerSX>
+        </Box>
+      </Box>
+      <Divider />
+    </>
   );
 }
