516a217b56a7c7a8fe61db8f7a5d61246cafd634
Revchenko Dmitry <reuchankadzmitry@gmail.com>
2023-02-09 23:05:35 +0300

    refactor: theme, remove unused fonts

--- src/modules/theme-module/themes/rootFontsTheme.ts
+++ src/modules/theme-module/themes/rootFontsTheme.ts
@@ -5,17 +5,6 @@
 
 export const rootFontsStyle = {
   typography: {
-    fontFamily: [
-      "-apple-system",
-      "BlinkMacSystemFont",
-      '"Segoe UI"',
-      "Roboto",
-      '"Helvetica Neue"',
-      "Arial",
-      "sans-serif",
-      '"Apple Color Emoji"',
-      '"Segoe UI Emoji"',
-      '"Segoe UI Symbol"',
-    ].join(","),
+    fontFamily: ['"Segoe UI Emoji"', '"Segoe UI Symbol"', "sans-serif"].join(","),
   },
 };
