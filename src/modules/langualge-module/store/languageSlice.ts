d629695e0d5cd4cfb591c40ecb7891fffa3edcea
Revchenko Dmitry <reuchankadzmitry@gmail.com>
2023-02-10 15:13:10 +0300

    feat: translation, transl. module + slice
    - create translate module;
    - create languagesSlice;

---
+++ src/modules/langualge-module/store/languageSlice.ts
@@ -0,0 +1,30 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { AvailableLanguagesType, changeLanguage } from "../../../shared/i18n/";
+
+interface LanguageState {
+  language: AvailableLanguagesType;
+}
+
+const LANGUAGE_LS_NAME = "language";
+
+const savedLanguage = (localStorage.getItem(LANGUAGE_LS_NAME) as AvailableLanguagesType) || "en";
+
+const initialState: LanguageState = {
+  language: savedLanguage,
+};
+
+export const languageSlice = createSlice({
+  name: "language",
+  initialState,
+  reducers: {
+    setLanguage: (state, action: PayloadAction<AvailableLanguagesType>) => {
+      state.language = action.payload;
+      changeLanguage(action.payload);
+      localStorage.setItem(LANGUAGE_LS_NAME, state.language);
+    },
+  },
+});
+
+export const { setLanguage } = languageSlice.actions;
+
+export const LanguageReducer = languageSlice.reducer;
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { ThemeReducer } from "../modules/theme-module";
+import { LanguageReducer } from "../modules/langualge-module";
 
 export const store = configureStore({
   reducer: {
     ThemeReducer,
+    LanguageReducer,
   },
 });
 
