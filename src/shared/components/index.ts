984886d2d8d3a13a7f3ca09a27ddafecbf50d201
Revchenko Dmitry <reuchankadzmitry@gmail.com>
2023-02-13 19:44:10 +0300

    refactor: shared, add sx prop to Button component and add all components to publick api

--- src/shared/components/index.ts
+++ src/shared/components/index.ts
@@ -1,2 +1,7 @@
 export * from "./Logo";
 export * from "./Button";
+export * from "./ValidatedInput";
+export * from "./ChildrenOrSpinner";
+export * from "./CustomLink";
+export * from "./LightTypo";
+export * from "./SecondaryHeadingTypo";
