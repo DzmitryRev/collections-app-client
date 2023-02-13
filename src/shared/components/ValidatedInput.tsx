772f698eeb7970cd174d63acf81af85ae5cd87da
Revchenko Dmitry <reuchankadzmitry@gmail.com>
2023-02-13 19:39:50 +0300

    feat: auth, add ValidatedInput component to shared

---
+++ src/shared/components/ValidatedInput.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { TextField, TextFieldProps } from "@mui/material";
+import { useTranslation } from "react-i18next";
+
+export type ValidatedInputPropsType = {
+  label: string;
+  touched: boolean | undefined;
+  errors: string | undefined;
+} & TextFieldProps;
+
+export function ValidatedInput({ label, touched, errors, ...args }: ValidatedInputPropsType) {
+  const { t } = useTranslation("translation");
+  return (
+    <div>
+      <TextField
+        label={t(label)}
+        name={label.toLowerCase()}
+        error={touched && !!errors}
+        helperText={t(errors || "") || " "}
+        {...args}
+      />
+    </div>
+  );
+}
