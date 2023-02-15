ef9cc7fc7c28c2f6e36127fd309f5ad8a05a193d
Revchenko Dmitry <reuchankadzmitry@gmail.com>
2023-02-15 16:04:50 +0300

    feat: shared/add ls contants and 2 paths in const/paths

--- src/shared/constants/paths.ts
+++ src/shared/constants/paths.ts
@@ -2,3 +2,4 @@ export const MAIN = "/";
 export const LOGIN = "/login";
 export const REGISTRATION = "/registration";
 export const CONFIRM_EMAIL = "/confirm-email";
+export const EMAIL_CONFIRMED = "/email-confirmed";
