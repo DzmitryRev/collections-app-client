b377c219f3e76dd4a89ae52b8b91bd1d9bb980c4
Revchenko Dmitry <reuchankadzmitry@gmail.com>
2023-02-15 12:12:17 +0300

    feat: auth/create useAuth hook and add pub. api for hooks dir

---
+++ src/modules/auth-module/hooks/useAuth.tsx
@@ -0,0 +1,23 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { useAppDispatch, useAppSelector } from "../../../store/hooks";
+import { cleanErrors } from "../store";
+
+export function useAuth() {
+  const { loading, errors } = useAppSelector((state) => state.AuthReducer);
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    return () => {
+      dispatch(cleanErrors());
+    };
+  }, []);
+
+  return {
+    loading,
+    errors,
+    dispatch,
+    navigate,
+  };
+}
