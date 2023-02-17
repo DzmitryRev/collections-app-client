562960dc47490d2a82dc2f332140789251fd73f7
Revchenko Dmitry <reuchankadzmitry@gmail.com>
2023-02-17 19:20:27 +0300

    refactor: restyling/change color pallete

--- src/modules/theme-module/themes/themes.ts
+++ src/modules/theme-module/themes/themes.ts
@@ -5,12 +5,6 @@ export const darkTheme = createTheme({
   ...rootFontsStyle,
   palette: {
     mode: "dark",
-    background: {
-      default: "#1B2021",
-    },
-    primary: {
-      main: "#33ab9f",
-    },
     error: {
       main: "#ec6565",
     },
@@ -21,11 +15,5 @@ export const lightTheme = createTheme({
   ...rootFontsStyle,
   palette: {
     mode: "light",
-    background: {
-      default: "#f6f8fc",
-    },
-    primary: {
-      main: "#009688",
-    },
   },
 });
