aa3b2212a545d4a8269bb312c2bda69f0bf9e0bd
Revchenko Dmitry <reuchankadzmitry@gmail.com>
2023-02-21 22:42:25 +0300

    feat: profile/implement UserMenu component

---
+++ src/modules/user-module/wigdet/UserMenu.tsx
@@ -0,0 +1,54 @@
+import React from "react";
+import { Box, IconButton, Menu, MenuItem } from "@mui/material";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import SettingsIcon from "@mui/icons-material/Settings";
+import Person2Icon from "@mui/icons-material/Person2";
+import { UserAvatar } from "../components/UserAvatar";
+import { Button, CustomLink } from "../../../shared/components";
+import { UserType } from "../../../shared/api";
+import { useMenu } from "../../../shared/hooks";
+
+interface IUserMenuProps {
+  user: UserType;
+  logout: () => void;
+}
+
+export function UserMenu({ user, logout }: IUserMenuProps) {
+  const [anchorEl, isOpen, openMenu, closeMenu] = useMenu();
+  return (
+    <Box sx={{ display: "flex", justifyContent: "center", alignItems: "center", m: 0 }}>
+      <IconButton sx={{ mr: 1 }} onClick={openMenu}>
+        <KeyboardArrowDownIcon sx={{ fontSize: "24px" }} />
+      </IconButton>
+      <Box>
+        <UserAvatar size="small" userName={user.name} src={user.avatar} />
+      </Box>
+
+      <Menu
+        id="basic-menu"
+        anchorEl={anchorEl}
+        open={isOpen}
+        onClose={closeMenu}
+        MenuListProps={{
+          "aria-labelledby": "basic-button",
+        }}
+      >
+        <CustomLink sx={{ color: "inherit" }} to={`/user/${user.id}`}>
+          <MenuItem onClick={closeMenu}>
+            <Person2Icon sx={{ mr: 1 }} /> Profile
+          </MenuItem>
+        </CustomLink>
+        <CustomLink sx={{ color: "inherit" }} to={`/settings`}>
+          <MenuItem onClick={closeMenu}>
+            <SettingsIcon sx={{ mr: 1 }} /> Settings
+          </MenuItem>
+        </CustomLink>
+        <MenuItem onClick={closeMenu}>
+          <Button variant="contained" color="error" fullWidth onClick={logout}>
+            Exit
+          </Button>
+        </MenuItem>
+      </Menu>
+    </Box>
+  );
+}
